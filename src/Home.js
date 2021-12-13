f6a454e143e70258fae1c74b27fc20a196cfe6d9
Samank Gupta <samankgupta@gmail.com>
2021-12-13 20:58:27 +0530

    notifications added

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import homeimage from "./assets/homeimage.png"
 import { supabase } from './supabaseClient'
 import { useNavigate } from 'react-router-dom'
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
 
 const HomePage = () => {
+
+    const [signedIn, setSignedIn] = React.useState(false)
 
     async function addMember() {
 
@@ -26,35 +30,48 @@ const HomePage = () => {
                 ])
         }
     }
+
     React.useEffect(() => {
         if (supabase.auth.user()) {
             addMember();
         }
     }, [supabase.auth.user()])
 
+    React.useEffect(() => {
+        if (signedIn) {
+            NotificationManager.success(`Welcome ${supabase.auth.user().user_metadata.name}!`, 'Successfully Signed In');
+            setSignedIn(false)
+        }
+    }, [signedIn])
+
     const navigate = useNavigate()
 
-    async function signInWithGoogle() {
+    const signInWithGoogle = async () => {
 
-        const { error } = await supabase.auth.signIn({
+        const { user } = await supabase.auth.signIn({
             provider: 'google',
         })
     }
+    supabase.auth.onAuthStateChange((event, session) => {
+        if (event === "SIGNED_IN")
+            setSignedIn(true)
+    })
     return (
-        <div className="grid grid-cols-2 pt-20 bg-blue-500 h-screen">
-            <div className="text-center mt-12 px-20">
+        <div className="grid grid-cols-1 lg:grid-cols-2 lg:pt-20 bg-blue-500 min-h-screen">
+            <div className="text-center mt-4 lg:mt-12 px-6 lg:px-20">
                 <h1 className="mt-12 mb-12 text-xl font-medium text-orange-900">Working from home makes it difficult for you to manage your tasks?<br /><br /><br /><span className="text-4xl">Introducting <span className="text-7xl bg-clip-text text-transparent bg-gradient-to-r from-orange-900 to-orange-600 filter drop-shadow hover:drop-shadow-xl">TaskHub</span>.</span><br /><br /><br />TaskHub helps you manage your individual/organizational tasks easily and efficiently.</h1>
-                <div className="flex justify-center">
+                <div className="flex justify-center mb-4">
                     {supabase.auth.user() ?
-                        <button onClick={() => navigate("/account")} className="flex items-center mt-4 py-2 px-6 bg-white shadow-md rounded-md text-gray-800 text-sm font-semibold ml-3 border border-gray-200 hover:shadow-xl text-2xl">Go To Account</button>
+                        <button onClick={() => navigate("/account")} className="flex items-center mt-4 py-2 px-6 bg-white shadow-md rounded-md text-gray-800 font-semibold ml-3 border border-gray-200 hover:shadow-xl text-2xl">Go To Account</button>
                         :
                         <button onClick={signInWithGoogle} className="flex items-center mt-4 py-2 px-6 bg-white shadow-md rounded-md text-gray-800 text-sm font-semibold ml-3 border border-gray-200 hover:shadow-xl"><img src="https://img.icons8.com/color/48/000000/google-logo.png" /><span className="ml-4 text-lg">Login with Google</span></button>
                     }
                 </div>
             </div >
-            <div className="mr-5">
+            <div className="lg:mr-5">
                 <img src={homeimage} alt="TaskHub" />
             </div>
+            <NotificationContainer />
         </div >
     );
 };
--- src/components/Navbar/Navbar/index.js
+++ src/components/Navbar/Navbar/index.js
@@ -21,23 +21,23 @@ export default function Navbar(props) {
   return (
     <div>
       <nav>
-        <div className="container cursor-default">
+        <div className="container">
           <div>
-            <h1 onClick={() => navigate("/")} className="text-3xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-orange-800 to-orange-600 hover:drop-shadow">TaskHub</h1>
+            <h1 onClick={() => navigate("/")} className="cursor-pointer text-3xl font-medium bg-clip-text text-transparent bg-gradient-to-r from-orange-700 to-orange-600 hover:drop-shadow">TaskHub</h1>
           </div>
           <div className="flex justify-end">
             <h2 className="font-bold">{supabase.auth.user() ? supabase.auth.user().user_metadata.name : ''}</h2>
-            <button onClick={signout} className="bg-blue-400 px-4 py-2 shadow-md rounded-md ml-3 hover:shadow-xl">Sign Out</button>
+            <button onClick={signout} className="bg-blue-500 px-4 py-2 shadow-md rounded-md ml-3 hover:shadow-xl">Sign Out</button>
           </div>
           {props.page === "personal" ?
             <div className="flex justify-end">
-              <h2 className="px-8 font-bold">Personal Tasks</h2>
-              <h2 onClick={() => navigate("/org")}>Organisation Tasks</h2>
+              <h2 className="px-8 font-bold cursor-pointer">Personal Tasks</h2>
+              <h2 onClick={() => navigate("/org")} className="cursor-pointer">Organisation Tasks</h2>
             </div>
             :
             <div className="flex justify-end">
-              <h2 onClick={() => navigate("/account")} className="px-8">Personal Tasks</h2>
-              <h2 className="font-bold">Organisation Tasks</h2>
+              <h2 onClick={() => navigate("/account")} className="px-8 cursor-pointer">Personal Tasks</h2>
+              <h2 className="font-bold cursor-pointer">Organisation Tasks</h2>
             </div>
           }
         </div>
